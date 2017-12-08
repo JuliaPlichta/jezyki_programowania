a82adbfd98e0a1286bacfc59e04912b2815185a3
Julia Plichta <32812897+JuliaPlichta@users.noreply.github.com>
2017-12-08 01:16:13 +0100

    Update zad2.c

--- lab4/zadpunktowane/zad2.c
+++ lab4/zadpunktowane/zad2.c
@@ -1,36 +1,19 @@
 #include <stdio.h>
 
-/*int funkcja(int a, int tab[2] int tab[2][5]){
-    if (b < 1) return 1;
-    else return funkcja(a, b - 1)*a;
-}
-*/
-
-int main() {
-  int dzies;
-//  int tab[5] = {1,2,3,4,5};
-//  int tab2[2][5] = {{1,2,3,4,5},{6,7,8,9,10}};
-//  for (int i=0; i<5; i++){
-//    printf("1: %d\n", tab2[0][i]);
-//    printf("2: %d\n", tab2[1][i]);
-//  }
-
-  printf("podaj liczbe: ");
-  scanf("%d", &dzies);
-
-while (dzies < 0) {
-
-  if (dzies%2 == 0) {
-    printf("%d, 0\n", dzies/2);
+void dzies_na_bin(int num){
+  if(num > 0){
+    dzies_na_bin(num / 2);
+    printf("%d", num % 2);
   }
-  else printf("1\n");
 }
 
-//  if (dzies%2 == 0) {
-//    printf("%d\n", dzies/2);
-//  }
-
-
+int main(){
+  int liczba;
+  printf("Podaj liczbę: ");
+  scanf("%d", &liczba);
+  printf("\nPostać binarna: ");
+  dzies_na_bin(liczba);
+  printf("\n");
 
   return 0;
 }
